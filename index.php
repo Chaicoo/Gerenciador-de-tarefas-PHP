648147460d0f74ef7aaf2c75d7918ceba38d258f
Chaicoo <gabriellimma27@gmail.com>
2021-10-25 18:41:29 -0300

    versão 5

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ if(isset($_GET['clear'])){
                         <script>
                             function deletar$key(){
                                 if(confirm('Confirmar remoção?')){
-                                    window.location = 'gerenciador-de-tarefas-php.vercel.app/task.php?key=$key';
+                                    window.location = 'https://gerenciador-de-tarefas-php-bcjr4w771-chico06.vercel.app/task.php?key=$key';
                                 }
                                 return false
                             }
